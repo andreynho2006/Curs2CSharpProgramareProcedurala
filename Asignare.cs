c7015b04eb0c9140ac902a9fd55793a90b5fc91f
andreynho2006 <andreynho2006@gmail.com>
2020-01-29 11:16:47 +0200

    Comentare cod si afisare rezultat ex.2

--- Asignare.cs
+++ Asignare.cs
@@ -6,6 +6,8 @@ namespace Curs2CSharpProgramareProcedurala
 	{
 		public static void Main(string[] args)
 		{
+            // Ex.1
+
 			int a = 3;
             // a = 3;
 			int b = (a = 2) * a;
@@ -15,6 +17,23 @@ namespace Curs2CSharpProgramareProcedurala
 			Console.WriteLine("a =" +a + ", b =" +b + ", c =" +c);
 			//Ce va afisa aceasta la executie si de ce ?
 			// a =2, b =5, c =20
+
+
+            // Ex. 2
+
+            double d = 2.95;
+			// declarare variabila de tip double;
+			int i = 4;
+			// declarare variabila de tip integer
+			Console.WriteLine(++d > i ? d : i);
+			// Ce se poate spune despre acesta?
+			// ++d returneaza 3.95; 3.95 < 4 => returneaza valoarea variabilei i adica 4;
+			// (daca ++d ar fi mai mare decat i ( i = 4 ) atunci ar returna valoarea variabilei d 
+			// Se compileaza?
+			// Da;
+			// Ce afiseaza?
+			// Afiseaza 4
+
 		}
 	}
 
