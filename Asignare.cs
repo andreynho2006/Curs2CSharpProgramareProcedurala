2eb480166d02c9e7018a3506acecffa5f90e45c1
andreynho2006 <andreynho2006@gmail.com>
2020-01-29 11:03:31 +0200

    Comentare cod si afisare rezultat

--- Asignare.cs
+++ Asignare.cs
@@ -7,9 +7,14 @@ namespace Curs2CSharpProgramareProcedurala
 		public static void Main(string[] args)
 		{
 			int a = 3;
+            // a = 3;
 			int b = (a = 2) * a;
+            // a = 2 b = 4
 			int c = b * (b = 5);
+            // a = 2; b = 4; apoi b = 5; si c = 4 * 5 = 20;
 			Console.WriteLine("a =" +a + ", b =" +b + ", c =" +c);
+			//Ce va afisa aceasta la executie si de ce ?
+			// a =2, b =5, c =20
 		}
 	}
 
