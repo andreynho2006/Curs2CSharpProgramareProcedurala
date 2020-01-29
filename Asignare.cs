0e66f856ca4e1fc0bde07eadc8e9e68c2896c0d6
andreynho2006 <andreynho2006@gmail.com>
2020-01-29 10:58:34 +0200

    curs 2 clasa Arhivare

---
+++ Asignare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Curs2CSharpProgramareProcedurala
+{
+	public class Asignare
+	{
+		public static void Main(string[] args)
+		{
+			int a = 3;
+			int b = (a = 2) * a;
+			int c = b * (b = 5);
+			Console.WriteLine("a =" +a + ", b =" +b + ", c =" +c);
+		}
+	}
+
+}
